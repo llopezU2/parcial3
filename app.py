69f42f308b42c350111adcdf249907cc82c12772
llopez2 <luisgamer6259@gmail.com>
2024-11-18 12:20:34 -0600

    no sirve

---
+++ app.py
@@ -0,0 +1,18 @@
+from derivada import primera_derivada
+
+def main():
+    # Valores de ejemplo para probar
+    f_values = {
+        'xi+2': 0.2,
+        'xi+1': 0.6363,
+        'xi-1': 0.1035,
+        'xi-2': 1.2
+    }
+    h = 0.25
+
+    # Calcular la derivada
+    resultado = primera_derivada(f_values, h)
+    print(f"El resultado de la derivada es: {resultado}")
+
+if __name__ == "__main__":
+    main()
